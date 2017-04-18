7cb387fa245cbfe5ecb8188c3db6a5405ebb6538
rmody3 <rmody3@gmail.com>
2017-04-17 20:07:18 -0400

    added jwt and account model and sessions, still need registrations

---
+++ app/models/account.rb
@@ -0,0 +1,14 @@
+class Account < ApplicationRecord
+
+
+  def self.from_token(token)
+    account_id = Auth.decode(token)["account_id"]
+    Account.find(account_id)
+  end
+
+  def self.authenticate(googleId)
+    account = Account.find_by(googleId: googleId)
+  end
+
+
+end
